28c7b612af29a22a3558257b886ec42939bdaef0
Mayank <Mayank@DESKTOP-N9F6762>
2025-01-06 23:01:04 +0530

    waits and dropdowns

---
+++ SeleniumPractice/src/test/java/waitsInSelenium/ExplicitWaits.java
@@ -0,0 +1,37 @@
+package waitsInSelenium;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class ExplicitWaits {
+	public static void main(String[] args) {
+WebDriverManager.chromedriver().setup();
+		
+		WebDriver driver=new ChromeDriver();
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		
+		driver.manage().window().maximize();
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+		String url="https://www.booking.com/";
+		driver.get(url);
+		WebElement popUp=wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//button[starts-with(@aria-label,'Dismiss')]")));
+		js.executeScript("arguments[0].setAttribute('style', 'background: yellow; border: 2px solid red;');", popUp);
+		popUp.click();
+		
+		WebElement register = wait.until(ExpectedConditions.elementToBeClickable(By.linkText("Register")));
+		js.executeScript("arguments[0].setAttribute('style', 'background: yellow; border: 2px solid red;');", register);
+
+		register.click();
+		driver.close();
+	}
+
+}
