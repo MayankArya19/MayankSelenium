454fbf19a616eb75129501d64ddf20ed40ed32c1
Mayank <Mayank@DESKTOP-N9F6762>
2025-01-07 08:58:26 +0530

    ACTIONS, CHECKBOXES

--- SeleniumPractice/src/test/java/waitsInSelenium/FluentWaits.java
+++ SeleniumPractice/src/test/java/waitsInSelenium/FluentWaits.java
@@ -11,7 +11,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.Wait;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
